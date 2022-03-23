77f878a064b432bbae9e03e3072f3bcc3a200080
elaine <e.cristina00@gmail.com>
2022-03-22 22:56:55 -0300

    Retirado o método  setId da classe Regiao

--- src/main/java/com/dio_class/devweek/Entity/Regiao.java
+++ src/main/java/com/dio_class/devweek/Entity/Regiao.java
@@ -25,9 +25,10 @@ public class Regiao {
         return id;
     }
 
-    public void setId(Long id) {
+    /*public void setId(Long id) {
         this.id = id;
-    }
+    }*/
+    // o método setID quem fará é o Spring
 
     public String getRegiao() {
         return regiao;
