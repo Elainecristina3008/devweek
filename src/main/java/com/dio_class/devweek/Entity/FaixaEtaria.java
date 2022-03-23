ea7188b161b0e0a6dd128ae2b804cb89f69ae8ee
elaine <e.cristina00@gmail.com>
2022-03-22 22:54:35 -0300

    preenchendo a classe FaixaEtaria

--- src/main/java/com/dio_class/devweek/Entity/FaixaEtaria.java
+++ src/main/java/com/dio_class/devweek/Entity/FaixaEtaria.java
@@ -26,10 +26,6 @@ public class FaixaEtaria {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public Long getFaixa_i() {
         return faixa_i;
     }
